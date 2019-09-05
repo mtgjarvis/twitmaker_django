0e0bc4a03fb95aca58f44b6bf472b10c8777d781
Mark Jarvis <mtgjarvis@gmail.com>
2019-09-05 14:52:24 -0400

    added delete button

--- twitmaker/static/js/main.js
+++ twitmaker/static/js/main.js
@@ -1,15 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
   let form = document.querySelector('form');
+  let tweets = document.querySelector('.tweets');
+  let li = document.createElement('li');
+  let deleteButton = docuemnt.querySelectorAll('#delete')
+
+
   form.addEventListener('submit', function(event) {
     event.preventDefault();
     let formData = new FormData(this);
     axios.post(
       this.action,
       formData,
-    ).then(function(response) {
-      console.log(response);
+      { headers: { "X-RequestedWith": "XMLHttpRequest" } }
+      )
+      .then(response => {
+        let date = new Date()
+        data = {
+          message: this.message.value,
+          date: date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear(),
+        }
+        tweets.innerHTML = ''
+        tweets.appendChild(li)
+        li.innerHTML = JSON.stringify(data)
+        console.log(date);
     }).catch(function(error) {
       console.log(error);
-    });
+    })
   });
-});
+  deleteButton.forEach(button => {
+    buttton.addEventListener('click', function() {
+      let b = buttton.parentNode.parentNode
+      b.remove()
+  })
+})});
--- twitmaker/views.py
+++ twitmaker/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from twitmaker.models import Tweet
 from twitmaker.forms import TweetForm
 
+
 def index(request):
     return render(request, 'index.html', {
         'tweets': Tweet.objects.all(), 
@@ -13,11 +14,12 @@ def index(request):
 def create_tweet(request):
     form = TweetForm(request.POST)
     tweet = form.instance
+
     if form.is_valid():
         form.save()
         return HttpResponseRedirect('/')
     else:
         return render(request, 'index.html', {
-            'tweets': Tweet.objects.all(), 
+            'tweets': Tweet.objects.all(),
             'form': form
         })
